e21b1c6f62a4494c0ce44be551a3ae1d034945d4
joel.carter <joel.carter@accenture.com>
2018-07-03 16:08:00 -0500

    Adding angular material to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderNavComponent } from './header-nav/header-nav.component';
 
+// import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule,matMenu} from '@angular/material';
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderNavComponent
   ],
   imports: [
-    BrowserModule
+   BrowserModule,
+   MatButtonModule, 
+   MatCheckboxModule,
+   matMenu,
+  //  BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
